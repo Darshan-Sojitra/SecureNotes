9f3dfa9de0acc1444d141003e138de3e6af217f5
Darshan-Sojitra <darshansojitra2003@gmail.com>
2026-02-10 11:27:37 +0530

    fix deployment errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,46 @@
 const dns = require("dns").promises;
-
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 require("dotenv").config();
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
+/* -------------------- CORS FIX -------------------- */
+const allowedOrigins = [
+    "https://secure-notes-u3ul.vercel.app"
+];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,OPTIONS"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    // Handle preflight
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+/* ------------------------------------------------- */
+
 connectDB();
 
-module.exports = app;
 app.get("/", (req, res) => {
     res.send("SecureNotes backend running");
 });
+
+module.exports = app;
